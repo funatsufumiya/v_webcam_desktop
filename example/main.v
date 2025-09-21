ebc65ba5ad4ccc8e4da2145703c556e57ebd70d9
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-09-21 13:22:27 +0900

    chore

--- example/main.v
+++ example/main.v
@@ -4,12 +4,16 @@ fn main(){
 	println("openpnp-capture version: ${webcam_desktop.library_version()}")
 
 	mut ctx := webcam_desktop.create_context()
+
 	devices := webcam_desktop.list_devices(ctx)
+	defer {
+		webcam_desktop.release_context(mut ctx)
+	}
+
 	if devices.len == 0 {
 		println('No device')
 	}
 	for device in devices {
 		println('${device.id}: Name: ${device.name}, Unique ID: ${device.unique_id}')
 	}
-	webcam_desktop.release_context(mut ctx)
 }
